b8730fccd3fd0fc91b26db69b80f2316f4702f03
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-13 23:55:38 +0200

    added simple ac plugin install script

---
+++ plugin/install.ps1
@@ -0,0 +1,16 @@
+# Just a simple script for copying plugin source code into
+# Assetto Corsa plugin folder
+
+$pluginName = "ACTC"
+$appsFolder = "C:\Program Files (x86)\Steam\steamapps\common\assettocorsa\apps\python"
+
+$pluginFolder = Join-Path -Path $appsFolder -Child $pluginName
+
+if(!(Test-Path -Path $pluginFolder))
+{
+    Write-Output "Creating plugin folder: $($pluginFolder)"
+    New-Item -Path $appsFolder -Name $pluginName -ItemType "directory"
+}
+
+Write-Output "Copying source files to $($pluginFolder)"
+Copy-Item -Path "./src/*" -Destination $pluginFolder -Recurse
