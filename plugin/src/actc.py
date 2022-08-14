5e0b4de402eeb3de6385aaf50e626648225bca2c
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-14 14:55:35 +0200

    read fuel info with sim_info

--- plugin/src/actc.py
+++ plugin/src/actc.py
@@ -1,6 +1,22 @@
-import sys
+import platform, sys, os
 import ac
 import acsys
+
+# obtain current architecture (64/32 bit)
+# we dont actually care about linkage
+arch, linkage = platform.architecture()
+if arch == "64bit":
+    sysdir = os.path.dirname(__file__) + "/third_party/stdlib64"
+else:
+    sysdir = os.path.dirname(__file__) + "/third_party/stdlib"
+sys.path.insert(0, sysdir)
+
+# this has to be here to correctly load _ctypes module
+# shamelessly stolen from Sidekick (https://www.racedepartment.com/downloads/sidekick.11007/)
+# source code
+os.environ["PATH"] = os.environ["PATH"] + ";."
+
+from third_party.sim_info import info
 
 updateTimer = 0.0
 
@@ -32,4 +48,5 @@ def acUpdate(deltaT):
     if updateTimer > 1.0:
         updateTimer = 0.0
         rpm = ac.getCarState(0, acsys.CS.RPM)
-        ac.console("RPM: " + str(rpm))
+        fuel = info.physics.fuel
+        ac.console("RPM: {}; Fuel: {}".format(rpm, fuel))
