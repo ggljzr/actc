cc8418953745fe2a3f4b181c063b7c7896ab9a9b
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-14 13:55:10 +0200

    get car name

--- plugin/src/actc.py
+++ plugin/src/actc.py
@@ -14,4 +14,8 @@ def acMain(ac_version):
     ac.console("AC Version: " + str(ac_version))
     ac.console("Python version: " + sys.version)
 
+    # 0 -> player's car
+    carName = ac.getCarName(0)
+    ac.console("Car name: " + carName)
+
     return "ACTC App"
