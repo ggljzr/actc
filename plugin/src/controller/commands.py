9d8b4709b59119c5ace658b6dbd1aecbe4f527f8
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-14 21:40:22 +0200

    implemented SetTC command

--- plugin/src/controller/commands.py
+++ plugin/src/controller/commands.py
@@ -2,6 +2,8 @@
 Module for serial interface commands to be used with the controller.
 """
 
+import struct
+
 
 class CommandBase:
     """
@@ -10,6 +12,9 @@ class CommandBase:
 
     Raises ``InvalidCommandException`` when constructor parameters
     do not match this format.
+
+    :param commandId: ID of the command, integer between 0 and 256 (one byte).
+    :param data: 4 bytes (e. g. ``bytes`` or ``bytearray`` or list integers between 0 and 256)
     """
 
     def __init__(self, commandId, data):
@@ -25,3 +30,27 @@ class CommandBase:
 
     def __bytes__(self):
         return bytes([self.commandId]) + self.data
+
+
+class IntegerCommand(CommandBase):
+    """
+    Command used for sending integers via serial interface. Uses ``struct.pack``
+    to create byte representation of given integer (using big endian).
+
+    :param value: should be integer (or castable to integer via ``int()``).
+    """
+
+    def __init__(self, commandId, value):
+        data = struct.pack(">I", int(value))
+        super().__init__(commandId, data)
+
+
+class SetTC(IntegerCommand):
+    """
+    Command for setting TC value. Command ID: 0x01.
+
+    :param value: value of the TC to be set, positive integer.
+    """
+
+    def __init__(self, value):
+        super().__init__(0x01, value)
