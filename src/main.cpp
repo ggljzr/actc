5243d0472654de62eee8be9b0410f38b81021d2a
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-24 23:51:00 +0200

    increased stack depth for serial task

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 void setup()
 {
     xTaskCreate(actcTasks::encoderTask, "encoderTask", 128, NULL, 1, NULL);
-    xTaskCreate(actcTasks::serialTask, "serialTask", 128, NULL, 2, NULL);
+    xTaskCreate(actcTasks::serialTask, "serialTask", 256, NULL, 2, NULL);
 }
 
 void loop()
