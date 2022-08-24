8b9cff20b8a11b8f128c5fd22a01f293e3a64385
Ondra Červenka <gogo.lejzr@gmail.com>
2022-08-25 00:05:13 +0200

    added note about serial task stack depth

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 void setup()
 {
     xTaskCreate(actcTasks::encoderTask, "encoderTask", 128, NULL, 1, NULL);
+
+    // Note that serialTask will become unstable at about 96 bytes of stack depth
+    // and I experienced some crashesh/freezing with 128 bytes of stack depth
     xTaskCreate(actcTasks::serialTask, "serialTask", 256, NULL, 2, NULL);
 }
 
